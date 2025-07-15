c1757d002bad549433caadb8544654d5dadab917
Adam Ignjatovic <ignjatovicadam@gmail.com>
2025-07-15 16:51:35 +0200

    feature: add vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import router from './router';
 import './assets/styles/_variables.css';
 import './assets/styles/_resets.css';
 import './assets/styles/_utils.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
