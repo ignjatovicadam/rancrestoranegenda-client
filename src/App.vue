28c697a4afd207153ea0a92a987a07572cddf75e
Adam Ignjatovic <ignjatovicadam@gmail.com>
2025-07-15 16:51:35 +0200

    feature: add vue-router

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
 
 <template>
   <div>
-    <span class="fs-secondary-heading">Ranc legenda</span>
+    <header>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+
+    <div>
+      <router-view />
+    </div>
   </div>
 </template>
