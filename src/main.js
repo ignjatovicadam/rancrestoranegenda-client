3276156b695840ca5f4120c5c124ef8b3c4691e1
Adam Ignjatovic <ignjatovicadam@gmail.com>
2025-07-20 14:35:51 +0200

    feature: add nav

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router';
 import './assets/styles/_variables.css';
 import './assets/styles/_resets.css';
 import './assets/styles/_utils.css';
+import './assets/styles/_general.css';
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,74 @@
 <script setup></script>
 
 <template>
-    <div>Home</div>
+    <section class="hero">
+        <div class="background-overlay"></div>
+        <div class="container">
+            <div class="welcome-text flow">
+                <h1 class="fs-primary-heading fw-bold">Lorem ipsum dolor sit amet.</h1>
+                <p class="fw-semi-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita autem nobis laborum consectetur accusamus nemo.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="padding-block-900">
+        <div class="container">
+            <div class="flow">
+                <h2 class="fs-secondary-heading fw-bold">Lorem ipsum dolor sit.</h2>
+                <p class="fw-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde impedit, repudiandae culpa voluptas dolores necessitatibus nobis dolore accusantium molestiae ad similique maiores est dicta aliquid ipsam in numquam! Amet, voluptas.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="services padding-block-900">
+        <div class="large-container">
+            <div class="even-columns">
+                <div>
+                    <a href="#" class="flow" style="--flow-spacer: 1em">
+                        <div class="image-overlay">
+                            <img src="https://july.finestwp.com/newwp/reston/wp-content/uploads/2024/10/dishes-1.jpg" alt="">
+                        </div>
+                        <h3 class="fs-tertiary-heading fw-bold">LOREM, IPSUM.</h3>
+                        <p class="fw-regular">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, rem?</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="flow" style="--flow-spacer: 1em">
+                        <div class="image-overlay">
+                            <img src="https://july.finestwp.com/newwp/reston/wp-content/uploads/2024/10/dishes-2.jpg" alt="">
+                        </div>
+                        <h3 class="fs-tertiary-heading fw-bold">LOREM, IPSUM.</h3>
+                        <p class="fw-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, numquam.</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="flow" style="--flow-spacer: 1em">
+                        <div class="image-overlay">
+                            <img src="https://july.finestwp.com/newwp/reston/wp-content/uploads/2024/10/quality-1.jpg" alt="">
+                        </div>
+                        <h3 class="fs-tertiary-heading fw-bold">LOREM, IPSUM.</h3>
+                        <p class="fw-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, praesentium?</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="flow" style="--flow-spacer: 1em">
+                        <div class="image-overlay">
+                            <img src="https://july.finestwp.com/newwp/reston/wp-content/uploads/2024/10/quality-2.jpg" alt="">
+                        </div>
+                        <h3 class="fs-tertiary-heading fw-bold">LOREM, IPSUM.</h3>
+                        <p class="fw-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptas!</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="flow" style="--flow-spacer: 1em">
+                        <div class="image-overlay">
+                            <img src="https://july.finestwp.com/newwp/reston/wp-content/uploads/2024/10/contact-1.jpg" alt="">
+                        </div>
+                        <h3 class="fs-tertiary-heading fw-bold">LOREM, IPSUM.</h3>
+                        <p class="fw-regular">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, itaque.</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
