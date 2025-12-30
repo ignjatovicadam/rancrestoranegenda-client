fa3be6477f35e9b97fbc7ab88e9c9796d17c25ff
Adam Ignjatovic <ignjatovicadam@gmail.com>
2025-12-30 19:11:32 +0100

    form fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import RestaurantView from '../views/RestaurantView.vue';
 import TeamBuildingView from '../views/TeamBuildingView.vue';
@@ -114,7 +114,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
     scrollBehavior() {
         return { top: 0 }
