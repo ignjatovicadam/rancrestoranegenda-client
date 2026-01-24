db69be13f5e78f0ad35a348bc44f5b0d06673b53
Adam Ignjatovic <ignjatovicadam@gmail.com>
2026-01-24 10:37:50 +0100

    testing d

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup>
   import { AppFooter, AppHeader, CookieConsent } from './components';
-
-  const robotsContent = import.meta.env.ROBOTS_META;
-  const metaTag = document.querySelector('meta[name="robots"]');
-  metaTag.setAttribute('content', robotsContent);
 </script>
 
 <template>
